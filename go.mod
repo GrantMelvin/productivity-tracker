a1e285a5b4a3b5285ef9a0dba72c16d13bf4fc42
GrantMelvin <Grantmelvin4@gmail.com>
2024-11-23 19:27:08 -0500

    lots

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.21.5
 require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,35 +3,95 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 )
 
 const SIZE = 500
+const DATA_DIR = "./assets/data"
 
-var days Days
+var logs Logs
 
 // Components of struct have to be capital for templating to work
 type Test struct {
 	Test string
 }
 
-type Day struct {
+type Log struct {
 	Date         string              `yaml:"date"`
 	Goals        []map[string]string `yaml:"goals"`
 	Productivity []map[string]string `yaml:"productivity"`
 	Notes        []map[string]string `yaml:"notes"`
 }
 
-type Days struct {
-	Days []Day
+type Logs struct {
+	Logs []Log
 }
 
-func getLogs(root_path string) Days {
+type SearchableLogs struct {
+	Logs   []Log
+	Search bool
+}
+
+func parseKeyValueArray(data []interface{}) []map[string]string {
+	result := []map[string]string{}
+
+	for _, item := range data {
+		if str, ok := item.(string); ok {
+			result = append(result, map[string]string{"": str})
+		} else if mapItem, ok := item.(map[string]interface{}); ok {
+			parsed := make(map[string]string)
+			for k, v := range mapItem {
+				if strValue, ok := v.(string); ok {
+					parsed[k] = strValue
+				}
+			}
+			result = append(result, parsed)
+		}
+	}
+
+	return result
+}
+
+func parseLogs(data []byte) (*Log, error) {
+	var raw map[string]interface{}
+	err := yaml.Unmarshal(data, &raw)
+	if err != nil {
+		return nil, err
+	}
+
+	Log := &Log{}
+
+	// Set the date field
+	if date, ok := raw["date"].(string); ok {
+		Log.Date = date
+	}
+
+	// Parse and set Goals
+	if goals, ok := raw["goals"].([]interface{}); ok {
+		Log.Goals = parseKeyValueArray(goals)
+	}
+
+	// Parse and set Productivity
+	if productivity, ok := raw["productivity"].([]interface{}); ok {
+		Log.Productivity = parseKeyValueArray(productivity)
+	}
+
+	// Parse and set Notes
+	if notes, ok := raw["notes"].([]interface{}); ok {
+		Log.Notes = parseKeyValueArray(notes)
+	}
+
+	return Log, nil
+}
+
+func getLogs(root_path string) Logs {
 
 	// Gets all subfolders of the data directory
 	entries, err := os.ReadDir(root_path)
@@ -39,54 +99,204 @@ func getLogs(root_path string) Days {
 		log.Fatal(err)
 	}
 
-	var total_file_count int = (len(entries))
-	fmt.Println("total file count:", total_file_count)
+	fmt.Println("total file count:", (len(entries)))
 
 	// Slice starting from 0 to the length of what we need
-	days_slice := Days{
-		Days: make([]Day, total_file_count, SIZE),
+	Logs_slice := Logs{
+		Logs: make([]Log, (len(entries)), SIZE),
 	}
 
-	for i, e := range entries {
-		filename := (root_path + "/" + e.Name())
+	Logs_slice = cleanLogs(Logs_slice, entries, root_path)
+
+	return Logs_slice
+}
+
+func cleanLogs(Logs Logs, file_entries []fs.DirEntry, root_path string) Logs {
+	for i, e := range file_entries {
+		filename := root_path + "/" + e.Name()
 		fmt.Println("Currently evaluating:", filename)
 
-		var day Day
 		current_file, err := os.ReadFile(filename)
 		if err != nil {
 			fmt.Println(err)
+			continue
 		}
 
-		err = yaml.Unmarshal(current_file, &day)
+		// Use the dynamic parsing function
+		Log, err := parseLogs(current_file)
 		if err != nil {
-			log.Fatalf("Unmarshal: %v", err)
+			fmt.Printf("Failed to parse file %s: %v\n", filename, err)
+			continue
 		}
 
-		days_slice.Days[i] = day
+		Logs.Logs[i] = *Log
 	}
 
-	return days_slice
+	// for Log := range Logs.Logs {
+	// 	fmt.Println(Logs.Logs[Log])
+	// }
 
+	return Logs
 }
 
-func home(w http.ResponseWriter, days Days) {
+func containsString(log Log, searchString string) bool {
+	// Easy check for data field
+	if strings.Contains(log.Date, searchString) {
+		return true
+	}
+
+	// Checks all of the mappings for the log we are evaluating
+	checkInSliceOfMaps := func(slice []map[string]string) bool {
+		// Checks each mapping
+		for _, m := range slice {
+
+			// Checks each key and value
+			for key, value := range m {
+				if strings.Contains(key, searchString) {
+					return true
+				}
+				if strings.Contains(value, searchString) {
+					return true
+				}
+			}
+		}
+		return false
+	}
+
+	// Check Goals, Productivity, and Notes fields
+	if checkInSliceOfMaps(log.Goals) || checkInSliceOfMaps(log.Productivity) || checkInSliceOfMaps(log.Notes) {
+		return true
+	}
+
+	// If not found in any field
+	return false
+}
+
+func writeLog(log Log) {
+	var fileName = DATA_DIR + "/" + log.Date + ".yaml"
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		fmt.Printf("error opening/creating file: %v\n", err)
+	}
+	fmt.Println("file:", file)
+	fmt.Println("log:", log)
+
+	defer file.Close()
+	enc := yaml.NewEncoder(file)
+
+	err = enc.Encode(log)
+
+	if err != nil {
+		fmt.Printf("error encoding: %v", err)
+	}
+}
+
+func searchLogs(keyword string) Logs {
+	var log_subset Logs
+	// fmt.Println(logs.Logs[0])
+	log_subset.Logs = []Log{}
+	for i, log := range logs.Logs {
+		log = logs.Logs[i]
+		fmt.Printf("Log %v: %v\n", i+1, log)
+
+		if containsString(log, keyword) {
+			log_subset.Logs = append(log_subset.Logs, log)
+		}
+	}
+	fmt.Println(log_subset)
+	return log_subset
+}
+
+func home(w http.ResponseWriter) {
 
 	var home = "home.html"
 
-	t, err := template.ParseFiles(home)
+	tmpl, err := template.ParseFiles(home)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(days.Days[0].Goals[0])
+	tmpl.ExecuteTemplate(w, home, SearchableLogs{Logs: logs.Logs, Search: false})
+}
 
-	t.ExecuteTemplate(w, home, Days{days.Days})
+func search(w http.ResponseWriter, r *http.Request) {
+	var search = "home.html"
+
+	tmpl, err := template.ParseFiles(search)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	keyword := r.FormValue("keyword")
+	fmt.Println(keyword)
+
+	// If they didnt search anything
+	// if keyword == "" {
+
+	// }
+
+	var subset = searchLogs(keyword)
+	fmt.Println("subet:", subset)
+
+	tmpl.ExecuteTemplate(w, search, SearchableLogs{Logs: subset.Logs, Search: true})
+}
+
+func add(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost { // Ensure the form was submitted as POST
+
+		// Parse and add goals, productivity, and notes from form values
+		var newLog Log
+		newLog.Date = r.FormValue("date")
+
+		// Parse goals
+		goalsKeys := r.Form["goals_key[]"]
+		goalsValues := r.Form["goals_value[]"]
+		for i := range goalsKeys {
+			newLog.Goals = append(newLog.Goals, map[string]string{goalsKeys[i]: goalsValues[i]})
+		}
+
+		// Parse productivity
+		productivityKeys := r.Form["productivity_key[]"]
+		productivityValues := r.Form["productivity_value[]"]
+		for i := range productivityKeys {
+			newLog.Productivity = append(newLog.Productivity, map[string]string{productivityKeys[i]: productivityValues[i]})
+		}
+
+		// Parse notes
+		notesKeys := r.Form["notes_key[]"]
+		notesValues := r.Form["notes_value[]"]
+		for i := range notesKeys {
+			newLog.Notes = append(newLog.Notes, map[string]string{notesKeys[i]: notesValues[i]})
+		}
+
+		// Add the new log to the logs slice
+		logs.Logs = append(logs.Logs, newLog)
+
+		writeLog(newLog)
+
+		// Redirect to the home page
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	// Render the add page for GET requests
+	var add = "add.html"
+	tmpl, err := template.ParseFiles(add)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	tmpl.ExecuteTemplate(w, add, nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
-		home(w, days)
+		home(w)
+	case "/search-logs":
+		search(w, r)
+	case "/add-logs":
+		add(w, r)
 	default:
 		fmt.Fprintf(w, "Hello")
 	}
@@ -103,8 +313,10 @@ func main() {
 	}
 
 	// retrieve the logs before we do anything
-	days = getLogs("./data")
+	logs = getLogs(DATA_DIR)
 
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets", fs))
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(server, nil)
 }
